23b2706d636d25fc76e931c984887cfc0baa98e3
Wenxiao Song <song.we@husky.neu.edu>
2016-08-10 14:52:41 -0400

    beginning

---
+++ blog/app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+var routes = require('./routes/index');
+var settings = require('./settings');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  store: new MongoStore({
+    url: 'mongodb://localhost/blog-reborn',
+    db: settings.db,
+    host: settings.host,
+    port: settings.port
+  })
+}));
+
+routes(app);
+
+app.listen(3000, function() {
+   console.log('Express server listening on port 3000');
+});
---
+++ blog/settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    cookieSecret: 'myblog',
+    db: 'blog-reborn',
+    host: 'localhost',
+    port: 27017
+};
